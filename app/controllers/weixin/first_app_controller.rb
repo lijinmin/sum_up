cf4c6c86c5180c92a93264ccdfeae21aa973427e
lijinmin <lijinmin@xjgreat.com>
2017-12-03 21:12:19 +0800

    m

--- app/controllers/weixin/first_app_controller.rb
+++ app/controllers/weixin/first_app_controller.rb
@@ -8,4 +8,10 @@ class Weixin::FirstAppController < Weixin::BaseController
 	def show
 		
 	end
+	private
+
+	# 该微信应用对应的secret
+	def app_secret
+		session[:secret] = Weixin::Configuration.first_app_secret
+	end
 end
--- lib/weixin/configuration.rb
+++ lib/weixin/configuration.rb
@@ -4,4 +4,8 @@ class Weixin::Configuration
 	config_accessor :corpid
 	# 微信回调域名
 	config_accessor :callback_domain
+	# 第一个微信应用对应的secret
+	config_accessor :first_app_secret
+	# access_token是企业后台去企业微信的后台获取信息时的重要票据
+	config_accessor :access_token
 end
