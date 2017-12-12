9b5227a16fabdf56174256a39319b04e9fa391e7
lijinmin <lijinmin@xjgreat.com>
2017-12-12 17:58:12 +0800

    m

--- config/initializers/base_config.rb
+++ config/initializers/base_config.rb
@@ -1,2 +1,5 @@
 #redis 配置
 $redis = Redis.new(:host => '127.0.0.1', :port => '6379', :db => 2)
+#es 配置
+$elastic = Elasticsearch::Client.new hosts: [{ host: '127.0.0.1',port: '9200',user: 'elastic',password: 'changeme'}], randomize_hosts: true, log: false
+Elasticsearch::Model.client = $elastic
